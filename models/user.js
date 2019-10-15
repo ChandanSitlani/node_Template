217843381aa813dc83c822b94c3332afd3198eb2
unknown <sitlanic@gmail.com>
2019-10-15 17:38:52 +0530

    new

---
+++ models/user.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
+
+var UserSchema = new mongoose.Schema({
+name :{type:String},
+rollno :{type:String},
+role : {type:String},
+age : {type:Number},
+email : {type:String},
+username : {type:String},
+collagename : {type:String}
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", UserSchema);
